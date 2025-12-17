df4e324ab039cb881d685b93eeef51cc23a9b626
lucas <lucassborgess600@gmail.com>
2025-12-17 12:40:33 -0300

    Deploy inicial

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- ADICIONE ISSO
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- E ISSO AQUI NA LISTA
-  ],
+  plugins: [react(), tailwindcss()],
+  
+  // ADICIONE ISSO AQUI:
+  // Troque 'nome-do-repositorio' pelo nome exato que você criou no GitHub
+  base: "https://github.com/Borgestar/QualityPrime"
 })
